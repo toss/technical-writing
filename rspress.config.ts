01c9376b43509f18d76186d580a4231c3c703bf5
Junseong Park <39112954+jsparkdev@users.noreply.github.com>
2025-04-25 23:37:41 +0900

    chore: update search placeholder to Korean (#5)

--- rspress.config.ts
+++ rspress.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
   globalStyles: path.join(__dirname, './styles/index.css'),
   themeConfig: {
     darkMode: false,
+    searchPlaceholderText: '검색',
     socialLinks: [
       {
         icon: 'github',
